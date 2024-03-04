d46f3f3112a755d5962fb43d90fe315d544c8a9a
aussiearef <arefkr@gmail.com>
2024-03-04 21:46:08 +1100

    Bug FIx

--- ShoeHubV2/Program.cs
+++ ShoeHubV2/Program.cs
@@ -1,42 +1,38 @@
 using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
-
 var app = builder.Build();
 
-
-var randomGenerator = new Random(DateTime.Now.Millisecond);
+var randomGenerator = new Random();
 
 var countryCodes = new[] { "AU", "US", "IN" };
 var paymentMethods = new[] { "Card", "Cash", "Paypal" };
 var shoeTypes = new[] { "Loafers", "Boots", "HighHeels" };
 
-Counter salesMetric;
-Gauge paymentMetric;
-
-#region Sales Metrics
-
-foreach (var shoeType in shoeTypes)
-{
-    var soldValue = randomGenerator.Next(10, 100);
-    salesMetric = Metrics.CreateCounter($"shoehub_sales_{shoeType}", $"Sales of {shoeType}.");
-    salesMetric.Inc(soldValue);
-}
-
-foreach (var countryCode in countryCodes)
-{
-    var paymentMethod = paymentMethods[randomGenerator.Next(paymentMethods.Length)];
-    var paymentValue = randomGenerator.Next(1000);
-
-    paymentMetric = Metrics.CreateGauge($"shoehub_{countryCode}_payments_{paymentMethod}", $"Payment in {countryCode}");
-    paymentMetric.Set(paymentValue);
-}
-
-#endregion
-
-
 app.UseHttpsRedirection();
 app.UseHttpMetrics();
-app.MapMetrics(pattern:"/").DisableRateLimiting();
+app.MapMetrics().DisableRateLimiting();
+
+app.Use(async (context, next) =>
+{
+    foreach (var shoeType in shoeTypes)
+    {
+        var soldValue = randomGenerator.Next(10, 100);
+        var salesMetric = Metrics.CreateCounter($"shoehub_sales_{shoeType}", $"Sales of {shoeType}.");
+        salesMetric.Inc(soldValue);
+    }
+
+    foreach (var countryCode in countryCodes)
+    {
+        foreach (var paymentMethod in paymentMethods)
+        {
+            var paymentValue = randomGenerator.Next(1000);
+            var paymentMetric = Metrics.CreateGauge($"shoehub_{countryCode}_payments_{paymentMethod}", $"Payment in {countryCode} using {paymentMethod}.");
+            paymentMetric.Set(paymentValue);
+        }
+    }
+
+    await next();
+});
 
 app.Run();
